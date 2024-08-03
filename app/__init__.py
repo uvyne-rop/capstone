027033c9fb75ed6530ac23cee424c62889aa3a36
uvyne-rop <ropchiri7@gmail.com>
2024-08-03 14:51:46 +0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_mail import Mail
+from flask_login import LoginManager
+from itsdangerous import URLSafeTimedSerializer
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from app.models import db  # Ensure your models are imported correctly
+
+app = Flask(__name__)
+
+
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+CORS(app)  # You can customize CORS here if needed
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+mail = Mail(app)
+login_manager = LoginManager(app)
+# Initialize the serializer with your app's secret key
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+from Backend.run import admin_routes  # Import routes after app is created
+# Create tables in the database
+with app.app_context():
+    db.create_all()
---
+++ config.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
+# from flask_session import Session
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager, decode_token
+
+# Load variables from .env file
+load_dotenv()
+
+app = Flask(__name__)
+# Configure the app with variables from the .env file
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS').lower() == 'true'
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL').lower() == 'true'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_MAX_EMAILS'] = os.getenv('MAIL_MAX_EMAILS')
+app.config['MAIL_ASCII_ATTACHMENTS'] = os.getenv('MAIL_ASCII_ATTACHMENTS').lower() == 'true'
+SESSION_TYPE = 'filesystem'
+
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'  # Directory to store uploaded images
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Maximum file size: 16MB
+
+# AWS S3 Configuration
+app.config['S3_BUCKET_NAME'] = os.getenv('S3_BUCKET_NAME')
+app.config['S3_REGION'] = os.getenv('S3_REGION')
+# app.config['S3_ACCESS_KEY'] = os.getenv('AWS_ACCESS_KEY_ID')
+# app.config['S3_SECRET_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECTET_KEY')
+jwt = JWTManager(app)
+
+
+# Session(app)
